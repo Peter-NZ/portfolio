8c6a2de530616dc309851f2b0e1e3f1c0d4b7908
Peter D <peterdavidson1997@gmail.com>
2020-02-15 20:33:53 +1300

    Added Initial LinkCard Component and Styles

---
+++ src/components/Common/Cards/LinkCard/LinkCard.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import classes from "./LinkCard.module.scss";
+import PropTypes from "prop-types";
+const LinkCard = props => {
+  return (
+    <Link href={props.link} className={classes.card}>
+      <div className={classes["card__top"]}>
+        <div className={classes["card__circle"]}>
+          <i className={`${classes["card__icon"]} material-icons`}>
+            {props.icon}
+          </i>
+        </div>
+      </div>
+      <h2 className={classes["card__title"]}>{props.title}</h2>
+      <p className={classes["card__text"]}>{props.subTitle}</p>
+    </Link>
+  );
+};
+
+export default LinkCard;
+
+LinkCard.propTypes = {
+  title: PropTypes.string.isRequired,
+  subTitle: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired,
+  link: PropTypes.string.isRequired,
+  backgroundGradient: PropTypes.string
+};
