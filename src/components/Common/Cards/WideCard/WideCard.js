3af6f2bbcd12418fae3752fdc7deee9d2fbb4a62
Peter D <peterdavidson1997@gmail.com>
2020-02-15 21:42:02 +1300

    Added Styles and Additional Markup

--- src/components/Common/Cards/WideCard/WideCard.js
+++ src/components/Common/Cards/WideCard/WideCard.js
@@ -1,33 +1,33 @@
-import React from 'react'
+import React from "react";
 import classes from "./LinkCard.module.scss";
 import PropTypes from "prop-types";
 
 const WideCard = () => {
-    const topBackground = {
-        backgroundImage: props.backgroundImage
-    };
-    return (
+  const topBackground = {
+    backgroundImage: props.backgroundImage
+  };
+  return (
     <section className={classes.card}>
-        <div style={topBackground} className={classes["card__side"]}>
-          <div className={classes["card__circle"]}>
-            <i className={`${classes["card__icon"]} material-icons`}>
-              {props.icon}
-            </i>
-          </div>
+      <div style={topBackground} className={classes["card__side"]}>
+        <div className={classes["card__circle"]}>
+          <i className={`${classes["card__icon"]} material-icons`}>
+            {props.icon}
+          </i>
         </div>
+      </div>
+      <div className={classes["card__content"]}>
         <h2 className={classes["card__title"]}>{props.title}</h2>
-        <p className={classes["card__text"]}>{props.subTitle}</p>
-      </section>
-    )
-}
+        <p className={classes["card__text"]}>{props.text}</p>
+      </div>
+    </section>
+  );
+};
 
-export default WideCard
-
+export default WideCard;
 
 LinkCard.propTypes = {
-    title: PropTypes.string.isRequired,
-    subTitle: PropTypes.string.isRequired,
-    icon: PropTypes.string.isRequired,
-    backgroundImage: PropTypes.string
+  title: PropTypes.string.isRequired,
+  text: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired,
+  backgroundImage: PropTypes.string
 };
-  
