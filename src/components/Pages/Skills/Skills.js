79f66ccc9e75a526a8214b970545b3203022fe11
Peter D <peterdavidson1997@gmail.com>
2020-02-16 19:49:42 +1300

    Initial Skills Grid Code

--- src/components/Pages/Skills/Skills.js
+++ src/components/Pages/Skills/Skills.js
@@ -1,8 +1,8 @@
 import React from "react";
 import Cockpit from "../../Common/Cockpit/Cockpit";
-
+import classes from "./Skills.module.scss";
 const Skills = () => {
-  const skills = [
+  const skillProps = [
     { name: "HTML5", emphasis: true },
     { name: "JavaScript(ES6)", emphasis: true },
     { name: "CSS", emphasis: true },
@@ -15,18 +15,34 @@ const Skills = () => {
     { name: "Angular Material", emphasis: false },
     { name: "SCSS", emphasis: false },
     { name: "TypeScript", emphasis: false },
-    { name: "VueJS", emphasis: false },
     { name: "Python", emphasis: false },
     { name: "VueJS", emphasis: false }
   ];
+  let startCol = 1;
+  const skills = skillProps.map((skill, i) => {
+    let row = 1;
+    if (i > 4) {
+      row = i < 9 ? 2 : 3;
+      if(startCol > 10){
+        startCol = row === 3 ? 1 : 2;
+      }
+    }
+    const gridPos = `${row} / ${startCol} / ${row} / ${startCol + 2}`;
+    startCol += 2;
+    return (
+      <div style={{ gridArea: gridPos }} className={classes["skill-hexagon"]}>
+        <span>{skill.name}</span>
+      </div>
+    );
+  });
 
-  
   return (
     <React.Fragment>
       <Cockpit
         title="Technical Skills"
         label="Some of the technical skills I've picked up in my time as a developer"
       ></Cockpit>
+      <div className={classes.skills}>{skills}</div>
     </React.Fragment>
   );
 };
