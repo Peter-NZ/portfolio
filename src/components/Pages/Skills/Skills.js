7c287ae4fa3fe025c37aa34f21220984af160d69
Peter D <peterdavidson1997@gmail.com>
2020-02-16 17:06:17 +1300

    initial Setup of Skills Page

--- src/components/Pages/Skills/Skills.js
+++ src/components/Pages/Skills/Skills.js
@@ -1,13 +1,34 @@
-import React from 'react'
+import React from "react";
 import Cockpit from "../../Common/Cockpit/Cockpit";
 
 const Skills = () => {
-    return (
-    <Cockpit
-      title="Technical Skills"
-      label="Some of the technical skills I've picked up in my time as a developer"
-    ></Cockpit>
-    )
-}
+  const skills = [
+    { name: "HTML5", emphasis: true },
+    { name: "JavaScript(ES6)", emphasis: true },
+    { name: "CSS", emphasis: true },
+    { name: "Angular", emphasis: true },
+    { name: "React", emphasis: true },
+    { name: "Git", emphasis: true },
+    { name: "Stylus", emphasis: true },
+    { name: "Angular CLI", emphasis: true },
+    { name: "Agile Methodology", emphasis: true },
+    { name: "Angular Material", emphasis: false },
+    { name: "SCSS", emphasis: false },
+    { name: "TypeScript", emphasis: false },
+    { name: "VueJS", emphasis: false },
+    { name: "Python", emphasis: false },
+    { name: "VueJS", emphasis: false }
+  ];
 
-export default Skills
+  
+  return (
+    <React.Fragment>
+      <Cockpit
+        title="Technical Skills"
+        label="Some of the technical skills I've picked up in my time as a developer"
+      ></Cockpit>
+    </React.Fragment>
+  );
+};
+
+export default Skills;
