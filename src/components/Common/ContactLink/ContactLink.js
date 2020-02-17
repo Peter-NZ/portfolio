e6721d63842a5df69b5f287f7f494a3f198680fe
Peter D <peterdavidson1997@gmail.com>
2020-02-17 21:02:09 +1300

    Adjusted Entry Animations to Make Them More Generic

---
+++ src/components/Common/ContactLink/ContactLink.js
@@ -0,0 +1,28 @@
+import React from "react";
+import PropTypes from "prop-types";
+import classes from './ContactLink.module.scss';
+const ContactLink = (props) => {
+  return (
+    <a
+      className={classes['contact']}
+      href={props.href}
+    >
+      <div className={classes['contact__circle']}>
+        <i className={[classes['contact__icon'], 'material-icons'].join(' ')}>{props.icon}</i>
+      </div>
+      <div>
+        <h2 className={classes['contact__heading']}>{props.title}</h2>
+        <a className={classes['contact__sub-title']}>{props.subTitle}</a>
+      </div>
+    </a>
+  );
+};
+
+export default ContactLink;
+
+ContactLink.prototypes = {
+  href: PropTypes.string,
+  title: PropTypes.string,
+  subTitle: PropTypes.string,
+  icon: PropTypes.string
+};
--- src/components/Pages/Contact/Contact.js
+++ src/components/Pages/Contact/Contact.js
@@ -1,12 +1,38 @@
 import React from "react";
 import Cockpit from "../../Common/Cockpit/Cockpit";
-
+import ContactLink from "../../Common/ContactLink/ContactLink";
+import classes from "./Contact.module.scss";
 const Contact = () => {
+  const contactLinkProps = [
+    {
+      href: "mailto:me@peterdavidson.dev",
+      title: "Email",
+      subTitle: "me@peterdavidson.dev",
+      icon: "mail_outline"
+    },
+    {
+      href: "https://www.linkedin.com/in/peteramdavidson",
+      title: "Linked In",
+      subTitle: "Peter Davidson",
+      icon: "perm_identity"
+    }
+  ];
+  const contactLinks = contactLinkProps.map(link => (
+    <ContactLink
+      title={link.title}
+      subTitle={link.subTitle}
+      href={link.href}
+      icon={link.icon}
+    ></ContactLink>
+  ));
   return (
-    <Cockpit
-      title="Contact Me"
-      label="Feel free to drop me a line on any of the bellow channels"
-    ></Cockpit>
+    <React.Fragment>
+      <Cockpit
+        title="Contact Me"
+        label="Feel free to drop me a line on any of the bellow channels"
+      ></Cockpit>
+      <div className={classes.links}>{contactLinks}</div>
+    </React.Fragment>
   );
 };
 
