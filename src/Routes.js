af57b6f5f1636e61aaa1c08e00a92f3af86ac7cc
Peter D <peterdavidson1997@gmail.com>
2020-06-29 19:39:55 +1200

    Updated Routes and Links to Use Routepaths Defined in const

--- src/Routes.js
+++ src/Routes.js
@@ -1,24 +1,37 @@
 import React from "react";
 import { Route } from "react-router-dom";
 import * as Pages from "./components/Pages";
+
+export const routePaths = {
+  home: "/",
+  about: "/about",
+  projects: "/projects",
+  skills: "/skills",
+  contact: "/contact",
+};
+
+export const routeLinks = [
+  { path: routePaths.home, title: "Home", icon: "home" },
+  { path: routePaths.about, title: "About Me", icon: "perm_identity" },
+  { path: routePaths.projects, title: "My Work", icon: "trending_up" },
+  { path: routePaths.skills, title: "Skills", icon: "whatshot" },
+  { path: routePaths.contact, title: "Contact Me", icon: "mail_outline" },
+];
+
 const Routes = () => {
   return (
     <React.Fragment>
-      <Route path="/" exact component={Pages.Home}></Route>
-      <Route path="/about" exact component={Pages.About}></Route>
-      <Route path="/projects" exact component={Pages.Projects}></Route>
-      <Route path="/skills" exact component={Pages.Skills}></Route>
-      <Route path="/contact" exact component={Pages.Contact}></Route>
+      <Route path={routePaths.home} exact component={Pages.Home}></Route>
+      <Route path={routePaths.about} exact component={Pages.About}></Route>
+      <Route path={routePaths.projects} exact component={Pages.Projects}></Route>
+      <Route path={routePaths.skills} exact component={Pages.Skills}></Route>
+      <Route path={routePaths.contact} exact component={Pages.Contact}></Route>
     </React.Fragment>
   );
 };
 
-export const routeLinks = [
-  { path: "/", title: "Home", icon: "home" },
-  { path: "/about", title: "About Me", icon: "perm_identity" },
-  { path: "/projects", title: "My Work", icon: "trending_up" },
-  { path: "/skills", title: "Skills", icon: "whatshot" },
-  { path: "/contact", title: "Contact Me", icon: "mail_outline" }
-];
+
+
+
 
 export default Routes;
