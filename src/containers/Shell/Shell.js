582eedc9dd77ee42ed44e1c1df740b777c3e3db8
Peter D <peterdavidson1997@gmail.com>
2021-02-02 21:32:55 +1300

    Moved Home Contents to Shell

--- src/containers/Shell/Shell.js
+++ src/containers/Shell/Shell.js
@@ -3,7 +3,11 @@ import Backdrop from "../../components/Backdrop/Backdrop";
 import Navigation from "../../components/Navigation/Navigation";
 import classes from "./Shell.module.scss";
 import LayoutContext from "../../context/LayoutContext";
-import Home from "../../components/Sections/Home/Home";
+import Hero from "../../components/Sections/Hero/Hero";
+import About from "../../components/Sections/About/About";
+import Projects from "../../components/Sections/Projects/Projects";
+import Skills from "../../components/Sections/Skills/Skills";
+import Contact from "../../components/Sections/Contact/Contact";
 import {
   routeContext as RouteContext,
   initialRouteContext,
@@ -29,8 +33,21 @@ export default class Shell extends Component {
             <Navigation />
           </LayoutContext.Provider>
           <main className={classes["center-wrapper"]}>
-            <Home/>
-          </main>
+          <div id="home">
+        <Hero />
+      </div>
+      <div id="about" className="section__wrapper section__wrapper--solid">
+        <About />
+      </div>
+      <div id="work" className="section__wrapper">
+        <Projects />
+      </div>
+      <div id="skills" className="section__wrapper">
+        <Skills />
+      </div>
+      <div id="contact" className="section__wrapper">
+        <Contact />
+      </div>          </main>
         </RouteContext.Provider>
         <Backdrop />
       </React.Fragment>
